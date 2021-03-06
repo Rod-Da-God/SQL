905bf40ecc455eaccdbe63709391b0da55cf4689
Rod-Da-God <stepan2281337228666@gmail.com>
2021-03-05 18:11:21 -0800

    #5

--- app/src/main/java/com/example/sql/GameThread.java
+++ app/src/main/java/com/example/sql/GameThread.java
@@ -83,6 +83,8 @@ class DrawThread extends Thread {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
+
             }
         }
     }
